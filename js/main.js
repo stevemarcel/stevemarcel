0ed9bf52fbf3216822c5f21c667bdc339482a7e1
stevemarcel <stevancy4@gmail.com>
2020-04-23 23:00:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 // Sticky navbar backgrond opacity
-window.addEventListener('scroll', function(){
-  if (window.scrollY > 100){
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
     document.querySelector('#navbar').style.opacity = 0.8;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
   }
-  else{
-    document.querySelector('#navbar').style.opacity = 1; 
+});
+
+// Smooth Scrolling
+$('.btn').on('click', (event) => {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
   }
-}
-);
+});
