48ed75e0b9dca64d53165fb24a75633482ab5d07
stevemarcel <stevancy4@gmail.com>
2020-06-21 15:20:00 +0100

    3rd init

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-// sidenav
+/*jshint esversion: 6*/
+// Sidenav
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
+
+// Fixed contact button
+document.addEventListener('DOMContentLoaded', function() {
+  const elems = document.querySelectorAll('.fixed-action-btn');
+  M.FloatingActionButton.init(elems, {});
+});
